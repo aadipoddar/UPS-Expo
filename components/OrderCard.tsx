5a4240ad92fde97aaa55990b5d2bf163732e9a9e
Aadi Poddar <83405769+aadipoddar@users.noreply.github.com>
2022-09-11 16:57:02 +0000

    Order Cards

---
+++ components/OrderCard.tsx
@@ -0,0 +1,51 @@
+import { ListItemSubtitle } from "@rneui/base/dist/ListItem/ListItem.Subtitle";
+import { Card, Icon } from "@rneui/themed";
+import { Text, TouchableOpacity, View } from "react-native";
+import { useTailwind } from "tailwind-rn/dist";
+
+type Props = {
+  item: Order;
+};
+
+const OrderCard = ({ item }: Props) => {
+  const tw = useTailwind();
+
+  return (
+    <TouchableOpacity>
+      <Card containerStyle={tw("px-5 rounded-lg")}>
+        <View style={tw("flex-row justify-between items-center")}>
+          <View>
+            <Icon
+              name="truck-delivery"
+              color={"#EB6A7C"}
+              type="material-community"
+            />
+
+            <Text style={{ fontSize: 10 }}>
+              {new Date(item.createdAt).toDateString()}
+            </Text>
+          </View>
+
+          <View>
+            <Text style={[tw("text-gray-400"), { fontSize: 10 }]}>
+              {item.carrier}-{item.trackingId}
+            </Text>
+
+            <Text style={tw("text-gray-500 text-xl")}>
+              {item.trackingItems.customer.name}
+            </Text>
+          </View>
+
+          <View style={tw("flex-row items-center")}>
+            <Text style={[tw("text-sm"), { color: "#EB6A7C" }]}>
+              {item.trackingItems.items.length} x
+            </Text>
+            <Icon style={tw("ml-2")} name="box" type="feather" />
+          </View>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+export default OrderCard;
--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,11 +1,72 @@
-import { View, Text } from 'react-native'
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Button, Image } from "@rneui/themed";
+import { useLayoutEffect, useState } from "react";
+import { Text, ScrollView, ActivityIndicator, View } from "react-native";
+import { useTailwind } from "tailwind-rn/dist";
+import OrderCard from "../components/OrderCard";
+import useOrders from "../hooks/useOrders";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { TabStackParamList } from "../navigator/TabNavigator";
+
+export type OrdersScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Orders">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 const OrdersScreen = () => {
-    return (
-        <View>
-            <Text>This is Orders Screen!</Text>
-        </View>
-    )
-}
+  const tw = useTailwind();
+  const navigation = useNavigation<OrdersScreenNavigationProp>();
+  const { loading, error, orders } = useOrders();
+  const [ascending, setAscending] = useState<boolean>(false);
 
-export default OrdersScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      tabBarLabel: ({ focused, color }) => (
+        <Text style={{ color: focused ? "#EB6A7C" : color, fontSize: 10 }}>
+          Orders
+        </Text>
+      ),
+    });
+  }, []);
+
+  return (
+    <ScrollView style={{ backgroundColor: "#EB6A7C" }}>
+      <Image
+        source={{ uri: "https://links.papareact.com/m51" }}
+        containerStyle={tw("w-full h-64")}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+
+      <View>
+        <Button
+          color="pink"
+          titleStyle={{ color: "gray", fontWeight: "400" }}
+          style={tw("py-2 px-5")}
+          onPress={() => setAscending(!ascending)}
+        >
+          {ascending ? "Showing: Oldest First" : "Showing: Newest First"}
+        </Button>
+
+        {orders
+          ?.sort((a, b) => {
+            if (ascending) {
+              return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+            } else {
+              return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+            }
+          })
+          .map((order) => (
+            <OrderCard key={order.trackingId} item={order} />
+          ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default OrdersScreen;
