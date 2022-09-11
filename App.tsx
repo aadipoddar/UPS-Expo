d576017482e9185d3bb00e6a080c3cbd596afcd9
Aadi Poddar <83405769+aadipoddar@users.noreply.github.com>
2022-09-11 12:47:41 +0000

    Connected Backend with Apollo Client

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
-import { TailwindProvider } from 'tailwind-rn'
-import utilities from './tailwind.json'
-import { NavigationContainer } from '@react-navigation/native'
-import RootNavigator from './navigator/RootNavigator'
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "./tailwind.json";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./navigator/RootNavigator";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5001/expo/ups",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
-  )
+  );
 }
--- typings.d.ts
+++ typings.d.ts
@@ -7,3 +7,37 @@ type CustomerList = {
     name: ID;
     value: Customer;
 }
+
+type TrackingItem = {
+    customer_id: ID;
+    customer: Customer;
+    items: Item[];
+}
+
+type Item = {
+    item_id: ID;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+type OrderResponse = {
+    value: Order;
+}
+
+type CustomerResponse = {
+    name: ID;
+    value: Customer;
+}
+
+type Order = {
+    carrier: string;
+    createdAt: string;
+    shippingCost: number;
+    trackingId: string;
+    trackingItems: TrackingItem;
+    Lat: number;
+    Lng: number;
+    Address: string;
+    City: string;
+}
